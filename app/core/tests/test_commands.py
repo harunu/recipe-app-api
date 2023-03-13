157b39084f67466ff81a97f10e08f98501291363
Harun <34203838+harunu@users.noreply.github.com>
2023-03-14 01:36:57 +0300

    Flake8 Fix!

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,6 @@ Test custom Django management commands.
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2OpError
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -31,4 +30,5 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
+
         patched_check.assert_called_with(databases=['default'])
